7dcd090039cd8138f7a40ce1c086387ae32550e8
Priyanka <priyanka.bundela03@hotmail.com>
2020-10-21 14:16:30 +0530

    my first commit

---
+++ src/test/java/test/GoogleExampleTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.io.File;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class GoogleExampleTest {
+	@Test
+	public void testcase()
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\bunde\\Downloads\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.google.com/");
+		TakesScreenshot tk = (TakesScreenshot)driver;
+		File src = tk.getScreenshotAs(OutputType.FILE);
+	}
+
+}
