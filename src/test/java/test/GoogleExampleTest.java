154c26fac0e32514e0be6a113bcab5afa44bf1be
Priyanka <priyanka.bundela03@hotmail.com>
2020-10-22 13:54:46 +0530

    master commit

--- src/test/java/test/GoogleExampleTest.java
+++ src/test/java/test/GoogleExampleTest.java
@@ -9,17 +9,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 
-
+//Master
 public class GoogleExampleTest {
 	@Test
 	public void testcase()
 	{
-<<<<<<< HEAD
-		
-		//commit in master
-=======
 
->>>>>>> TestingBranch
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\bunde\\Downloads\\chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
