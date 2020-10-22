408543d63f72fca2e7c6414371614acafd49c1b2
Priyanka <priyanka.bundela03@hotmail.com>
2020-10-22 13:51:58 +0530

    merge commit

--- src/test/java/test/GoogleExampleTest.java
+++ src/test/java/test/GoogleExampleTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-
+//Branch//
 
 public class GoogleExampleTest {
 	@Test
